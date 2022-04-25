7c93dd2cf76b6302fb69238580902fdc427f94fb
Chris Ayers <clayers@gmail.com>
2022-04-25 14:24:50 -0400

    Added global usings

--- DependenSee/PowerArgsProgram.cs
+++ DependenSee/PowerArgsProgram.cs
@@ -1,20 +1,18 @@
-﻿using PowerArgs;
+﻿namespace DependenSee;
 
-namespace DependenSee
+public enum OutputTypes
 {
-    public enum OutputTypes
-    {
-        [ArgDescription("Creates an html document.")] Html,
-        [ArgDescription("Creates a JSON file.")] Json,
-        [ArgDescription("Creates a XML file.")] Xml,
-        [ArgDescription("Creates a Graphviz/DOT file.")] Graphviz,
-        [ArgDescription("Writes JSON output to stdout")] ConsoleJson,
-        [ArgDescription("Writes XML output to stdout")] ConsoleXml,
-        [ArgDescription("Writes Graphviz output to stdout")] ConsoleGraphviz,
-    }
+    [ArgDescription("Creates an html document.")] Html,
+    [ArgDescription("Creates a JSON file.")] Json,
+    [ArgDescription("Creates a XML file.")] Xml,
+    [ArgDescription("Creates a Graphviz/DOT file.")] Graphviz,
+    [ArgDescription("Writes JSON output to stdout")] ConsoleJson,
+    [ArgDescription("Writes XML output to stdout")] ConsoleXml,
+    [ArgDescription("Writes Graphviz output to stdout")] ConsoleGraphviz,
+}
 
-    [ArgExceptionBehavior(ArgExceptionPolicy.StandardExceptionHandling)]
-    [ArgDescription(@"
+[ArgExceptionBehavior(ArgExceptionPolicy.StandardExceptionHandling)]
+[ArgDescription(@"
 
 
     ==================================================================
@@ -24,72 +22,71 @@ namespace DependenSee
     |                                                                |
     ==================================================================
 ")]
-    public class PowerArgsProgram
+public class PowerArgsProgram
+{
+    [HelpHook]
+    [ArgDescription("Shows help descriptions.")]
+    public bool Help { get; set; }
+    [ArgRequired]
+    [ArgPosition(0)]
+    [ArgDescription("Root folder (usually solution folder) to look for csproj files recursively.")]
+    [ArgExistingDirectory]
+    public string SourceFolder { get; set; }
+
+    [ArgDescription("Path to write the result. Not required if writing the output to stdout")]
+    [ArgPosition(1)]
+    public string OutputPath { get; set; }
+
+    [ArgDefaultValue(false)]
+    [ArgDescription("Whether to include external (Nuget) packages in the result.")]
+    [ArgShortcut("P")]
+    public bool IncludePackages { get; set; }
+
+    [ArgDefaultValue(OutputTypes.Html)]
+    [ArgDescription("Type of the output.")]
+    [ArgShortcut("T")]
+    public OutputTypes OutputType { get; set; }
+    
+    [ArgDefaultValue("DependenSee")]
+    [ArgDescription("Document title for Html output. Ignored for other output types.")]
+    [ArgShortcut("HT")]
+    public string HtmlTitle { get; set; }
+
+    [ArgDefaultValue("")]
+    [ArgDescription("Comma separated list of project file prefixes to include. Wildcards not allowed. Only the filename is considered, case insensitive. Ex:'MyApp.Core, MyApp.Extensions' Includes only projects starting with MyApp.Core and projects starting with MyApp.Extensions")]
+    [ArgShortcut("IPrN")]
+    public string IncludeProjectNamespaces { get; set; }
+
+    [ArgDescription("Comma separated list of project file prefixes to exclude. Wildcards not allowed. Only the filename is considered, case insensitive. This must be a subset of includes to be useful. Ex: 'MyApp.Extensions, MyApp.Helpers' Excludes projects starting with MyApp.Extensions and projects starting with MyApp.Helpers")]
+    [ArgShortcut("EPrN")]
+    public string ExcludeProjectNamespaces { get; set; }
+
+    [ArgDefaultValue("")]
+    [ArgDescription("Comma separated list of package name prefixes to include. Wildcards not allowed. Only the package name is considered, case insensitive. If specified, 'IncludePackages' is overridden to True. Ex: 'Xamarin, Microsoft' includes only packages starting with Xamarin and packages starting with Microsoft")]
+    [ArgShortcut("IPaN")]
+    public string IncludePackageNamespaces { get; set; }
+
+    [ArgDescription("Comma separated list of package name prefixes to exclude. Wildcards not allowed. Only the filename is considered, case insensitive. If specified, 'IncludePackages' is overridden to True. This must be a subset of includes to be useful. Ex: 'Microsoft.Logging, Azure' Excludes packages starting with Microsoft.Logging and packages starting with Azure")]
+    [ArgShortcut("EPaN")]
+    public string ExcludePackageNamespaces { get; set; }
+
+    public void Main()
     {
-        [HelpHook]
-        [ArgDescription("Shows help descriptions.")]
-        public bool Help { get; set; }
-        [ArgRequired]
-        [ArgPosition(0)]
-        [ArgDescription("Root folder (usually solution folder) to look for csproj files recursively.")]
-        [ArgExistingDirectory]
-        public string SourceFolder { get; set; }
-
-        [ArgDescription("Path to write the result. Not required if writing the output to stdout")]
-        [ArgPosition(1)]
-        public string OutputPath { get; set; }
-
-        [ArgDefaultValue(false)]
-        [ArgDescription("Whether to include external (Nuget) packages in the result.")]
-        [ArgShortcut("P")]
-        public bool IncludePackages { get; set; }
-
-        [ArgDefaultValue(OutputTypes.Html)]
-        [ArgDescription("Type of the output.")]
-        [ArgShortcut("T")]
-        public OutputTypes OutputType { get; set; }
-        
-        [ArgDefaultValue("DependenSee")]
-        [ArgDescription("Document title for Html output. Ignored for other output types.")]
-        [ArgShortcut("HT")]
-        public string HtmlTitle { get; set; }
-
-        [ArgDefaultValue("")]
-        [ArgDescription("Comma separated list of project file prefixes to include. Wildcards not allowed. Only the filename is considered, case insensitive. Ex:'MyApp.Core, MyApp.Extensions' Includes only projects starting with MyApp.Core and projects starting with MyApp.Extensions")]
-        [ArgShortcut("IPrN")]
-        public string IncludeProjectNamespaces { get; set; }
-
-        [ArgDescription("Comma separated list of project file prefixes to exclude. Wildcards not allowed. Only the filename is considered, case insensitive. This must be a subset of includes to be useful. Ex: 'MyApp.Extensions, MyApp.Helpers' Excludes projects starting with MyApp.Extensions and projects starting with MyApp.Helpers")]
-        [ArgShortcut("EPrN")]
-        public string ExcludeProjectNamespaces { get; set; }
-
-        [ArgDefaultValue("")]
-        [ArgDescription("Comma separated list of package name prefixes to include. Wildcards not allowed. Only the package name is considered, case insensitive. If specified, 'IncludePackages' is overridden to True. Ex: 'Xamarin, Microsoft' includes only packages starting with Xamarin and packages starting with Microsoft")]
-        [ArgShortcut("IPaN")]
-        public string IncludePackageNamespaces { get; set; }
-
-        [ArgDescription("Comma separated list of package name prefixes to exclude. Wildcards not allowed. Only the filename is considered, case insensitive. If specified, 'IncludePackages' is overridden to True. This must be a subset of includes to be useful. Ex: 'Microsoft.Logging, Azure' Excludes packages starting with Microsoft.Logging and packages starting with Azure")]
-        [ArgShortcut("EPaN")]
-        public string ExcludePackageNamespaces { get; set; }
-
-        public void Main()
+        var service = new ReferenceDiscoveryService
         {
-            var service = new ReferenceDiscoveryService
-            {
-                ExcludePackageNamespaces = ExcludePackageNamespaces,
-                ExcludeProjectNamespaces = ExcludeProjectNamespaces,
-                IncludePackageNamespaces = IncludePackageNamespaces,
-                IncludeProjectNamespaces = IncludeProjectNamespaces,
+            ExcludePackageNamespaces = ExcludePackageNamespaces,
+            ExcludeProjectNamespaces = ExcludeProjectNamespaces,
+            IncludePackageNamespaces = IncludePackageNamespaces,
+            IncludeProjectNamespaces = IncludeProjectNamespaces,
 
-                IncludePackages = IncludePackages,
+            IncludePackages = IncludePackages,
 
-                OutputPath = OutputPath,
-                OutputType = OutputType,
+            OutputPath = OutputPath,
+            OutputType = OutputType,
 
-                SourceFolder = SourceFolder,
-            };
-            var result = service.Discover();
-            new ResultWriter().Write(result, OutputType, OutputPath, HtmlTitle);
-        }
+            SourceFolder = SourceFolder,
+        };
+        var result = service.Discover();
+        new ResultWriter().Write(result, OutputType, OutputPath, HtmlTitle);
     }
 }
--- DependenSee/ReferenceDiscoveryService.cs
+++ DependenSee/ReferenceDiscoveryService.cs
@@ -1,180 +1,173 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Xml;
+﻿namespace DependenSee;
 
-namespace DependenSee
+public class ReferenceDiscoveryService
 {
-    public class ReferenceDiscoveryService
+    public string SourceFolder { get; set; }
+    public string OutputPath { get; set; }
+    public bool IncludePackages { get; set; }
+    public OutputTypes OutputType { get; set; }
+    public string IncludeProjectNamespaces { get; set; }
+    public string ExcludeProjectNamespaces { get; set; }
+    public string IncludePackageNamespaces { get; set; }
+    public string ExcludePackageNamespaces { get; set; }
+
+    private string[] _includeProjectNamespaces { get; set; }
+    private string[] _excludeProjectNamespaces { get; set; }
+    private string[] _includePackageNamespaces { get; set; }
+    private string[] _excludePackageNamespaces { get; set; }
+
+    private bool _shouldIncludePackages { get; set; }
+
+    public DiscoveryResult Discover()
     {
-        public string SourceFolder { get; set; }
-        public string OutputPath { get; set; }
-        public bool IncludePackages { get; set; }
-        public OutputTypes OutputType { get; set; }
-        public string IncludeProjectNamespaces { get; set; }
-        public string ExcludeProjectNamespaces { get; set; }
-        public string IncludePackageNamespaces { get; set; }
-        public string ExcludePackageNamespaces { get; set; }
-
-        private string[] _includeProjectNamespaces { get; set; }
-        private string[] _excludeProjectNamespaces { get; set; }
-        private string[] _includePackageNamespaces { get; set; }
-        private string[] _excludePackageNamespaces { get; set; }
-
-        private bool _shouldIncludePackages { get; set; }
-
-        public DiscoveryResult Discover()
+        var result = new DiscoveryResult
         {
-            var result = new DiscoveryResult
-            {
-                Packages = new List<Package>(),
-                Projects = new List<Project>(),
-                References = new List<Reference>()
-            };
+            Packages = new List<Package>(),
+            Projects = new List<Project>(),
+            References = new List<Reference>()
+        };
 
-            _includeProjectNamespaces = ParseStringToLowercaseStringArray(IncludeProjectNamespaces);
-            _excludeProjectNamespaces = ParseStringToLowercaseStringArray(ExcludeProjectNamespaces);
+        _includeProjectNamespaces = ParseStringToLowercaseStringArray(IncludeProjectNamespaces);
+        _excludeProjectNamespaces = ParseStringToLowercaseStringArray(ExcludeProjectNamespaces);
 
-            _includePackageNamespaces = ParseStringToLowercaseStringArray(IncludePackageNamespaces);
-            _excludePackageNamespaces = ParseStringToLowercaseStringArray(ExcludePackageNamespaces);
+        _includePackageNamespaces = ParseStringToLowercaseStringArray(IncludePackageNamespaces);
+        _excludePackageNamespaces = ParseStringToLowercaseStringArray(ExcludePackageNamespaces);
 
-            if (!_includeProjectNamespaces.Any()) _includeProjectNamespaces = new[] { "" };
-            if (!_includePackageNamespaces.Any()) _includePackageNamespaces = new[] { "" };
-            _shouldIncludePackages = IncludePackages
-                || !string.IsNullOrWhiteSpace(IncludePackageNamespaces)
-                || !string.IsNullOrWhiteSpace(ExcludePackageNamespaces);
+        if (!_includeProjectNamespaces.Any()) _includeProjectNamespaces = new[] { "" };
+        if (!_includePackageNamespaces.Any()) _includePackageNamespaces = new[] { "" };
+        _shouldIncludePackages = IncludePackages
+            || !string.IsNullOrWhiteSpace(IncludePackageNamespaces)
+            || !string.IsNullOrWhiteSpace(ExcludePackageNamespaces);
 
-            Discover(SourceFolder, result);
-            return result;
-        }
+        Discover(SourceFolder, result);
+        return result;
+    }
 
-        private static string[] ParseStringToLowercaseStringArray(string list) =>
-            string.IsNullOrWhiteSpace(list)
-            ? Array.Empty<string>()
-            : list.Split(',', StringSplitOptions.RemoveEmptyEntries)
-                .Select(e => e.Trim().ToLower())
-                .ToArray();
+    private static string[] ParseStringToLowercaseStringArray(string list) =>
+        string.IsNullOrWhiteSpace(list)
+        ? Array.Empty<string>()
+        : list.Split(',', StringSplitOptions.RemoveEmptyEntries)
+            .Select(e => e.Trim().ToLower())
+            .ToArray();
 
-        private void Discover(string folder, DiscoveryResult result)
+    private void Discover(string folder, DiscoveryResult result)
+    {
+        var projectFiles = Directory.EnumerateFiles(folder, "*.csproj")
+            .Concat(Directory.EnumerateFiles(folder, "*.vbproj"));
+        foreach (var file in projectFiles)
         {
-            var projectFiles = Directory.EnumerateFiles(folder, "*.csproj")
-                .Concat(Directory.EnumerateFiles(folder, "*.vbproj"));
-            foreach (var file in projectFiles)
+            var id = file.Replace(SourceFolder, "");
+            var name = Path.GetFileNameWithoutExtension(file);
+            if (!_includeProjectNamespaces.Any(i => name.ToLower().StartsWith(i))) continue;
+            if (_excludeProjectNamespaces.Any(i => name.ToLower().StartsWith(i))) continue;
+
+            // add this project.
+            if (!result.Projects.Any(p => p.Id == id))
+                result.Projects.Add(new Project
+                {
+                    Id = id,
+                    Name = name
+                });
+
+            var (projects, packages) = DiscoverFileReferences(file);
+
+            projects = projects
+                    .Where(p =>
+                        _includeProjectNamespaces.Any(i => p.Name.ToLower().StartsWith(i))
+                        && !_excludeProjectNamespaces.Any(i => p.Name.ToLower().StartsWith(i)))
+                    .ToList();
+
+            foreach (var project in projects)
             {
-                var id = file.Replace(SourceFolder, "");
-                var name = Path.GetFileNameWithoutExtension(file);
-                if (!_includeProjectNamespaces.Any(i => name.ToLower().StartsWith(i))) continue;
-                if (_excludeProjectNamespaces.Any(i => name.ToLower().StartsWith(i))) continue;
+                if (!result.Projects.Any(p => p.Id == project.Id)) result.Projects.Add(project);
 
-                // add this project.
-                if (!result.Projects.Any(p => p.Id == id))
-                    result.Projects.Add(new Project
-                    {
-                        Id = id,
-                        Name = name
-                    });
-
-                var (projects, packages) = DiscoverFileReferences(file);
-
-                projects = projects
-                        .Where(p =>
-                            _includeProjectNamespaces.Any(i => p.Name.ToLower().StartsWith(i))
-                            && !_excludeProjectNamespaces.Any(i => p.Name.ToLower().StartsWith(i)))
-                        .ToList();
-
-                foreach (var project in projects)
+                result.References.Add(new Reference
                 {
-                    if (!result.Projects.Any(p => p.Id == project.Id)) result.Projects.Add(project);
-
-                    result.References.Add(new Reference
-                    {
-                        From = id,
-                        To = project.Id
-                    });
-                }
-
-                if (!_shouldIncludePackages) continue;
-
-                packages = packages.Where(p =>
-                {
-                    return _includePackageNamespaces.Any(i => p.Name.ToLower().StartsWith(i))
-                        && !_excludePackageNamespaces.Any(i => p.Name.ToLower().StartsWith(i));
-                }).ToList();
-
-                foreach (var package in packages)
-                {
-                    if (!result.Packages.Any(p => p.Id == package.Id)) result.Packages.Add(package);
-
-                    result.References.Add(new Reference
-                    {
-                        From = id,
-                        To = package.Id
-                    });
-                }
-            }
-            var directories = Directory.EnumerateDirectories(folder);
-            foreach (var directory in directories)
-            {
-                Discover(directory, result);
-            }
-        }
-
-        private (List<Project> projects, List<Package> packages) DiscoverFileReferences(string path)
-        {
-            var xml = new XmlDocument();
-            xml.Load(path);
-            var basePath = new FileInfo(path).Directory.FullName;
-
-            var projects = DiscoverProjectRefrences(xml, basePath);
-            var packages = _shouldIncludePackages
-                ? DiscoverPackageReferences(xml)
-                : null;
-
-            return (projects, packages);
-        }
-
-        private List<Package> DiscoverPackageReferences(XmlDocument xml)
-        {
-            // PackageReference = Nuget package
-            // Reference = COM/DLL reference. These can have a child <HintPath>relative path to dll</HintPath>'
-            // Reference also present for .NET Framework projects when they reference BCL assemblies, but these
-            // do not include a HintPath
-            var packageReferenceNodes = xml.SelectNodes("//*[local-name() = 'PackageReference' or local-name() = 'Reference']");
-            var packages = new List<Package>();
-            foreach (XmlNode node in packageReferenceNodes)
-            {
-                var packageName = node.Attributes["Include"]?.Value
-                               ?? node.Attributes["Update"].Value;
-
-                packages.Add(new Package
-                {
-                    Id = packageName,
-                    Name = packageName
+                    From = id,
+                    To = project.Id
                 });
             }
-            return packages;
-        }
 
-        private List<Project> DiscoverProjectRefrences(XmlDocument xml, string basePath)
-        {
-            var projectReferenceNodes = xml.SelectNodes("//*[local-name() = 'ProjectReference']");
-            var projects = new List<Project>();
+            if (!_shouldIncludePackages) continue;
 
-            foreach (XmlNode node in projectReferenceNodes)
+            packages = packages.Where(p =>
             {
-                var referencePath = node.Attributes["Include"].Value;
-                var fullPath = Path.GetFullPath(referencePath, basePath);
+                return _includePackageNamespaces.Any(i => p.Name.ToLower().StartsWith(i))
+                    && !_excludePackageNamespaces.Any(i => p.Name.ToLower().StartsWith(i));
+            }).ToList();
 
-                string filename = Path.GetFileNameWithoutExtension(fullPath);
+            foreach (var package in packages)
+            {
+                if (!result.Packages.Any(p => p.Id == package.Id)) result.Packages.Add(package);
 
-                projects.Add(new Project
+                result.References.Add(new Reference
                 {
-                    Id = fullPath.Replace(SourceFolder, ""),
-                    Name = filename
+                    From = id,
+                    To = package.Id
                 });
             }
-            return projects;
         }
+        var directories = Directory.EnumerateDirectories(folder);
+        foreach (var directory in directories)
+        {
+            Discover(directory, result);
+        }
+    }
+
+    private (List<Project> projects, List<Package> packages) DiscoverFileReferences(string path)
+    {
+        var xml = new XmlDocument();
+        xml.Load(path);
+        var basePath = new FileInfo(path).Directory.FullName;
+
+        var projects = DiscoverProjectRefrences(xml, basePath);
+        var packages = _shouldIncludePackages
+            ? DiscoverPackageReferences(xml)
+            : null;
+
+        return (projects, packages);
+    }
+
+    private List<Package> DiscoverPackageReferences(XmlDocument xml)
+    {
+        // PackageReference = Nuget package
+        // Reference = COM/DLL reference. These can have a child <HintPath>relative path to dll</HintPath>'
+        // Reference also present for .NET Framework projects when they reference BCL assemblies, but these
+        // do not include a HintPath
+        var packageReferenceNodes = xml.SelectNodes("//*[local-name() = 'PackageReference' or local-name() = 'Reference']");
+        var packages = new List<Package>();
+        foreach (XmlNode node in packageReferenceNodes)
+        {
+            var packageName = node.Attributes["Include"]?.Value
+                           ?? node.Attributes["Update"].Value;
+
+            packages.Add(new Package
+            {
+                Id = packageName,
+                Name = packageName
+            });
+        }
+        return packages;
+    }
+
+    private List<Project> DiscoverProjectRefrences(XmlDocument xml, string basePath)
+    {
+        var projectReferenceNodes = xml.SelectNodes("//*[local-name() = 'ProjectReference']");
+        var projects = new List<Project>();
+
+        foreach (XmlNode node in projectReferenceNodes)
+        {
+            var referencePath = node.Attributes["Include"].Value;
+            var fullPath = Path.GetFullPath(referencePath, basePath);
+
+            string filename = Path.GetFileNameWithoutExtension(fullPath);
+
+            projects.Add(new Project
+            {
+                Id = fullPath.Replace(SourceFolder, ""),
+                Name = filename
+            });
+        }
+        return projects;
     }
 }
