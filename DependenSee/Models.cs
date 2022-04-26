7f5914eb6eda4dcdfbb4aeebbb80f2ef78cc4870
Chris Ayers <clayers@gmail.com>
2022-04-26 01:11:51 -0400

    Leverage additional .NET 6 features (#18)
    * Added global usings
    
    * replaced newtonsoft.json with system.text.json

--- DependenSee/Models.cs
+++ DependenSee/Models.cs
@@ -1,28 +1,25 @@
-﻿using System.Collections.Generic;
+﻿namespace DependenSee;
 
-namespace DependenSee
+public class DiscoveryResult
 {
-    public class DiscoveryResult
-    {
-        public List<Project> Projects { get; set; }
-        public List<Package> Packages { get; set; }
-        public List<Reference> References { get; set; }
-    }
+    public List<Project> Projects { get; set; }
+    public List<Package> Packages { get; set; }
+    public List<Reference> References { get; set; }
+}
 
-    public class Project
-    {
-        public string Id { get; set; }
-        public string Name { get; set; }
-    }
-    public class Package
-    {
-        public string Id { get; set; }
-        public string Name { get; set; }
-    }
+public class Project
+{
+    public string Id { get; set; }
+    public string Name { get; set; }
+}
+public class Package
+{
+    public string Id { get; set; }
+    public string Name { get; set; }
+}
 
-    public class Reference
-    {
-        public string From { get; set; }
-        public string To { get; set; }
-    }
+public class Reference
+{
+    public string From { get; set; }
+    public string To { get; set; }
 }
--- DependenSee/ResultWriter.cs
+++ DependenSee/ResultWriter.cs
@@ -1,104 +1,98 @@
-﻿using Newtonsoft.Json;
-using System;
-using System.IO;
-using System.Net;
-using System.Reflection;
-using System.Xml.Serialization;
+﻿namespace DependenSee;
 
-namespace DependenSee
+internal class ResultWriter
 {
-    internal class ResultWriter
+    private const string HtmlTemplateToken = "'{#SOURCE_TOKEN#}'";
+    private const string HtmlTitleToken = "{#TITLE_TOKEN#}";
+    private const string HtmlTemplateName = "HtmlResultTemplate.html";
+    private JsonSerializerOptions _JsonSerializerOptions = new JsonSerializerOptions() { WriteIndented = true };
+
+    internal void Write(DiscoveryResult result, OutputTypes type, string outputPath, string htmlTitle)
     {
-        private const string HtmlTemplateToken = "'{#SOURCE_TOKEN#}'";
-        private const string HtmlTitleToken = "{#TITLE_TOKEN#}";
-        private const string HtmlTemplateName = "HtmlResultTemplate.html";
-        internal void Write(DiscoveryResult result, OutputTypes type, string outputPath, string htmlTitle)
+        switch (type)
         {
-            switch (type)
-            {
-                case OutputTypes.Html:
-                case OutputTypes.Xml:
-                case OutputTypes.Json:
-                case OutputTypes.Graphviz:
-                    if (string.IsNullOrWhiteSpace(outputPath))
-                    {
-                        Console.Error.WriteLine($"output type {type} require specifying {nameof(outputPath)}");
-                        return;
-                    }
-                    break;
-            }
-
-            switch (type)
-            {
-                case OutputTypes.Html:
-                    WriteAsHtmlToFile(result, outputPath, htmlTitle);
-                    break;
-                case OutputTypes.Xml:
-                    WriteAsXmlToFile(result, outputPath);
-                    break;
-                case OutputTypes.Json:
-                    WriteAsJsonToFile(result, outputPath);
-                    break;
-                case OutputTypes.Graphviz:
-                    WriteAsGraphvizToFile(result, outputPath);
-                    break;
-                case OutputTypes.ConsoleJson:
-                    WriteAsJsonToConsole(result);
-                    break;
-                case OutputTypes.ConsoleXml:
-                    WriteAsXmlToConsole(result);
-                    break;
-                case OutputTypes.ConsoleGraphviz:
-                    WriteAsGraphvizToConsole(result);
-                    break;
-                default:
-                    throw new Exception($"Unknown {nameof(type)} '{type}'");
-            }
+            case OutputTypes.Html:
+            case OutputTypes.Xml:
+            case OutputTypes.Json:
+            case OutputTypes.Graphviz:
+                if (string.IsNullOrWhiteSpace(outputPath))
+                {
+                    Console.Error.WriteLine($"output type {type} require specifying {nameof(outputPath)}");
+                    return;
+                }
+                break;
         }
 
-        private void WriteAsHtmlToFile(DiscoveryResult result, string outputPath, string title)
+        switch (type)
         {
-            var templatePath = Path.Combine(new FileInfo(Assembly.GetExecutingAssembly().Location).DirectoryName, HtmlTemplateName);
-            var template = File.ReadAllText(templatePath);
-            var html = template
-                .Replace(HtmlTemplateToken, JsonConvert.SerializeObject(result, Formatting.Indented))
-                .Replace(HtmlTitleToken, WebUtility.HtmlEncode(title));
-
-            File.WriteAllText(outputPath, html);
-            Console.WriteLine($"HTML output written to {outputPath}");
+            case OutputTypes.Html:
+                WriteAsHtmlToFile(result, outputPath, htmlTitle);
+                break;
+            case OutputTypes.Xml:
+                WriteAsXmlToFile(result, outputPath);
+                break;
+            case OutputTypes.Json:
+                WriteAsJsonToFile(result, outputPath);
+                break;
+            case OutputTypes.Graphviz:
+                WriteAsGraphvizToFile(result, outputPath);
+                break;
+            case OutputTypes.ConsoleJson:
+                WriteAsJsonToConsole(result);
+                break;
+            case OutputTypes.ConsoleXml:
+                WriteAsXmlToConsole(result);
+                break;
+            case OutputTypes.ConsoleGraphviz:
+                WriteAsGraphvizToConsole(result);
+                break;
+            default:
+                throw new Exception($"Unknown {nameof(type)} '{type}'");
         }
-
-        private void WriteAsXmlToConsole(DiscoveryResult result)
-        {
-            var serializer = new XmlSerializer(typeof(DiscoveryResult));
-            using var writeStream = Console.Out;
-            serializer.Serialize(writeStream, result);
-        }
-
-        private void WriteAsXmlToFile(DiscoveryResult result, string outputPath)
-        {
-            var serializer = new XmlSerializer(typeof(DiscoveryResult));
-            using var writeStream = File.OpenWrite(outputPath);
-            serializer.Serialize(writeStream, result);
-            Console.WriteLine($"XML output written to {outputPath}");
-        }
-
-        private void WriteAsJsonToFile(DiscoveryResult result, string outputPath)
-        {
-            File.WriteAllText(outputPath, JsonConvert.SerializeObject(result, Formatting.Indented));
-            Console.WriteLine($"JSON output written to {outputPath}");
-        }
-        private void WriteAsJsonToConsole(DiscoveryResult result) =>
-            Console.WriteLine(JsonConvert.SerializeObject(result, Formatting.Indented));
-
-        private static void WriteAsGraphvizToConsole(DiscoveryResult result) 
-            => Console.WriteLine(GraphvizSerializer.ToString(result));
-
-        private static void WriteAsGraphvizToFile(DiscoveryResult result, string outputPath)
-        {
-            File.WriteAllText(outputPath, GraphvizSerializer.ToString(result));
-            Console.WriteLine($"GraphViz output written to {outputPath}");
-        }
-
     }
+
+    private void WriteAsHtmlToFile(DiscoveryResult result, string outputPath, string title)
+    {
+        var templatePath = Path.Combine(new FileInfo(Assembly.GetExecutingAssembly().Location).DirectoryName, HtmlTemplateName);
+        var template = File.ReadAllText(templatePath);
+        var html = template
+            .Replace(HtmlTemplateToken, JsonSerializer.Serialize(result, _JsonSerializerOptions))
+            .Replace(HtmlTitleToken, WebUtility.HtmlEncode(title));
+
+        File.WriteAllText(outputPath, html);
+        Console.WriteLine($"HTML output written to {outputPath}");
+    }
+
+    private void WriteAsXmlToConsole(DiscoveryResult result)
+    {
+        var serializer = new XmlSerializer(typeof(DiscoveryResult));
+        using var writeStream = Console.Out;
+        serializer.Serialize(writeStream, result);
+    }
+
+    private void WriteAsXmlToFile(DiscoveryResult result, string outputPath)
+    {
+        var serializer = new XmlSerializer(typeof(DiscoveryResult));
+        using var writeStream = File.OpenWrite(outputPath);
+        serializer.Serialize(writeStream, result);
+        Console.WriteLine($"XML output written to {outputPath}");
+    }
+
+    private void WriteAsJsonToFile(DiscoveryResult result, string outputPath)
+    {
+        File.WriteAllText(outputPath, JsonSerializer.Serialize(result, _JsonSerializerOptions));
+        Console.WriteLine($"JSON output written to {outputPath}");
+    }
+    private void WriteAsJsonToConsole(DiscoveryResult result) =>
+        Console.WriteLine(JsonSerializer.Serialize(result, _JsonSerializerOptions));
+
+    private static void WriteAsGraphvizToConsole(DiscoveryResult result)
+        => Console.WriteLine(GraphvizSerializer.ToString(result));
+
+    private static void WriteAsGraphvizToFile(DiscoveryResult result, string outputPath)
+    {
+        File.WriteAllText(outputPath, GraphvizSerializer.ToString(result));
+        Console.WriteLine($"GraphViz output written to {outputPath}");
+    }
+
 }
