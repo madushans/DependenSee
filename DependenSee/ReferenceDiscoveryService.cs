1b89a253a422f59e11ec376fda8666724db180ad
Madushan <madushan@outlook.com>
2023-01-23 22:35:24 +1300

    Revert "[Feature] Discover from solution file(s) (#27)"
    This reverts commit 5dea4b999147dc4684947ad4d0693cded437f354.

--- DependenSee/ReferenceDiscoveryService.cs
+++ DependenSee/ReferenceDiscoveryService.cs
@@ -16,15 +16,12 @@ public class ReferenceDiscoveryService
     public string ExcludeProjectsPattern { get; set; }
     public bool FollowReparsePoints { get; set; }
     public string ExcludeFolders { get; set; }
-    public string SolutionFiles { get; set; }
 
     private string[] _includeProjectNamespaces { get; set; }
     private string[] _excludeProjectNamespaces { get; set; }
     private string[] _includePackageNamespaces { get; set; }
     private string[] _excludePackageNamespaces { get; set; }
     private Matcher _findProjectsPattern { get; set; }
-
-    private string[] _solutionFileNames { get; set; }
 
     private bool _shouldIncludePackages { get; set; }
 
@@ -43,12 +40,6 @@ public class ReferenceDiscoveryService
         _includePackageNamespaces = ParseStringToLowercaseStringArray(IncludePackageNamespaces);
         _excludePackageNamespaces = ParseStringToLowercaseStringArray(ExcludePackageNamespaces);
 
-
-        _solutionFileNames = ParseStringToLowercaseStringArray(SolutionFiles);
-
-        if (!_includeProjectNamespaces.Any()) _includeProjectNamespaces = new[] { "" };
-        if (!_includePackageNamespaces.Any()) _includePackageNamespaces = new[] { "" };
-
         _findProjectsPattern = new Matcher(StringComparison.OrdinalIgnoreCase)
             .AddInclude("*.csproj")
             .AddInclude("*.vbproj");
@@ -62,24 +53,11 @@ public class ReferenceDiscoveryService
             _includeProjectNamespaces = new[] { "" };
         if (!_includePackageNamespaces.Any())
             _includePackageNamespaces = new[] { "" };
-
         _shouldIncludePackages = IncludePackages
             || !string.IsNullOrWhiteSpace(IncludePackageNamespaces)
             || !string.IsNullOrWhiteSpace(ExcludePackageNamespaces);
 
-        if (SolutionFiles.Length > 0)
-        {
-            var solutionFiles = GetValidatedSolutionFiles(SourceFolder);
-            if (solutionFiles != null && solutionFiles.Count > 0)
-            {
-                Discover(solutionFiles, result);
-            }
-        }
-        else
-        {
-            Discover(SourceFolder, result);
-        }
-
+        Discover(SourceFolder, result);
         return result;
     }
 
@@ -89,90 +67,6 @@ public class ReferenceDiscoveryService
         : list.Split(',', StringSplitOptions.RemoveEmptyEntries)
             .Select(e => e.Trim().ToLower())
             .ToArray();
-
-    private List<string> GetValidatedSolutionFiles(string folder)
-    {
-        var solutionFilesInFolder = GetSolutionFilesInFolder(folder);
-
-        if (solutionFilesInFolder.Count == 0)
-        {
-            Console.Error.WriteLine($"No solution files found in '{folder}'\r\n\r\n");
-            return null;
-        }
-
-        var foundSolutionFiles = (from sfif in solutionFilesInFolder
-                                  from sf in _solutionFileNames
-                                  where sfif.EndsWith(sf, StringComparison.OrdinalIgnoreCase)
-                                  select sfif).ToList();
-
-        if (foundSolutionFiles.Count != _solutionFileNames.Length)
-        {
-            Console.Error.WriteLine($"Solution files specified in SolutionFiles parameter were not all found\r\n\r\n");
-            return null;
-        }
-
-        return foundSolutionFiles;
-    }
-
-    private List<string> GetSolutionFilesInFolder(string folder)
-    {
-        var info = new DirectoryInfo(folder);
-        return (from fileInfo in info.GetFiles("*.sln")
-                select fileInfo.FullName).ToList();
-    }
-
-    private void Discover(IEnumerable<string> solutionFiles, DiscoveryResult result)
-    {
-        var projectFiles = DiscoverProjectsFromSolutionFiles(solutionFiles);
-        foreach (var file in projectFiles)
-        {
-            DiscoverSingleProject(file, result);
-        }
-    }
-
-    private List<string> DiscoverProjectsFromSolutionFiles(IEnumerable<string> solutionFiles)
-    {
-        var projects = new List<string>();
-
-        foreach (var solutionFile in solutionFiles)
-        {
-            var solutionProjects = DiscoverProjectsFromSolutionFile(solutionFile);
-
-            projects.AddRange(solutionProjects.Except(projects));
-        }
-
-        return projects;
-    }
-
-    private List<string> DiscoverProjectsFromSolutionFile(string solutionFile)
-    {
-        /*
-         * Finding lines in solution file poiting to C# or VB.NET projects. The lines look like this:
-         *
-         *   Project("{9A19103F-16F7-4668-BE54-9A1E7A4F7556}") = "SampleProject", "SampleProject\SampleProject.csproj", "{0FBD58A5-1EEE-4D36-AB37-AD6CC8252191}"
-         *
-         * First let splits this line into two by the '=' sign and trim leading and trailing whitespace.
-         * Second let splits the part after = sign by the ',' and trim leading and trailing whitespace.
-         * Finally returning the project file full path from the second splits second element after removing the leading and trailing " signs.
-         */
-        var solutionLines = ReadSolutionFileLines(solutionFile);
-        return (from solutionLine in solutionLines
-                where solutionLine.Contains(".csproj", StringComparison.OrdinalIgnoreCase) ||
-                      solutionLine.Contains(".vbproj", StringComparison.OrdinalIgnoreCase)
-                let lineParts = solutionLine.Split("=".ToCharArray(), StringSplitOptions.TrimEntries)
-                let projectInfo = lineParts[1].Split(",".ToCharArray(), StringSplitOptions.TrimEntries)
-                select Path.Combine(SourceFolder, projectInfo[1].Trim('"'))).ToList();
-    }
-
-    private string[] ReadSolutionFileLines(string solutionFile)
-    {
-        using (var streamReader = new StreamReader(solutionFile))
-        {
-            var contents = streamReader.ReadToEnd();
-            // Trim lines and remove empty entries as these are not needed.
-            return contents.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
-        }
-    }
 
     private void Discover(string folder, DiscoveryResult result)
     {
@@ -200,10 +94,6 @@ public class ReferenceDiscoveryService
 
         foreach (var file in projectFiles)
         {
-
-            DiscoverSingleProject(file, result);
-        }
-
             var id = file.Replace(SourceFolder, "");
             var name = Path.GetFileNameWithoutExtension(file);
             if (!_includeProjectNamespaces.Any(i => name.ToLower().StartsWith(i)))
@@ -221,50 +111,35 @@ public class ReferenceDiscoveryService
 
             var (projects, packages) = DiscoverFileReferences(file);
 
+            projects = projects
+                    .Where(p =>
+                        _includeProjectNamespaces.Any(i => p.Name.ToLower().StartsWith(i))
+                        && !_excludeProjectNamespaces.Any(i => p.Name.ToLower().StartsWith(i)))
+                    .ToList();
 
-        var directories = Directory.EnumerateDirectories(folder);
-        foreach (var directory in directories)
-        {
-            Discover(directory, result);
-        }
-    }
-
-    private void DiscoverSingleProject(string file, DiscoveryResult result)
-    {
-        var id = file.Replace(SourceFolder, "");
-        var name = Path.GetFileNameWithoutExtension(file);
-        if (!_includeProjectNamespaces.Any(i => name.ToLower().StartsWith(i))) return;
-        if (_excludeProjectNamespaces.Any(i => name.ToLower().StartsWith(i))) return;
-
-        // add this project.
-        if (!result.Projects.Any(p => p.Id == id))
-            result.Projects.Add(new Project
+            foreach (var project in projects)
             {
-                Id = id,
-                Name = name
-            });
                 if (!result.Projects.Any(p => p.Id == project.Id))
                     result.Projects.Add(project);
 
-        var (projects, packages) = DiscoverFileReferences(file);
+                result.References.Add(new Reference
+                {
+                    From = id,
+                    To = project.Id
+                });
+            }
 
-        projects = projects
-                .Where(p =>
-                    _includeProjectNamespaces.Any(i => p.Name.ToLower().StartsWith(i))
-                    && !_excludeProjectNamespaces.Any(i => p.Name.ToLower().StartsWith(i)))
-                .ToList();
             if (!_shouldIncludePackages)
                 continue;
 
-        foreach (var project in projects)
-        {
-            if (!result.Projects.Any(p => p.Id == project.Id)) result.Projects.Add(project);
-
-            result.References.Add(new Reference
+            packages = packages.Where(p =>
             {
-                From = id,
-                To = project.Id
-            });
+                return _includePackageNamespaces.Any(i => p.Name.ToLower().StartsWith(i))
+                    && !_excludePackageNamespaces.Any(i => p.Name.ToLower().StartsWith(i));
+            }).ToList();
+
+            foreach (var package in packages)
+            {
                 if (!result.Packages.Any(p => p.Id == package.Id))
                     result.Packages.Add(package);
 
@@ -275,24 +150,10 @@ public class ReferenceDiscoveryService
                 });
             }
         }
-
-        if (!_shouldIncludePackages) return;
-
-        packages = packages.Where(p =>
+        var directories = Directory.EnumerateDirectories(folder);
+        foreach (var directory in directories)
         {
-            return _includePackageNamespaces.Any(i => p.Name.ToLower().StartsWith(i))
-                && !_excludePackageNamespaces.Any(i => p.Name.ToLower().StartsWith(i));
-        }).ToList();
-
-        foreach (var package in packages)
-        {
-            if (!result.Packages.Any(p => p.Id == package.Id)) result.Packages.Add(package);
-
-            result.References.Add(new Reference
-            {
-                From = id,
-                To = package.Id
-            });
+            Discover(directory, result);
         }
     }
 
